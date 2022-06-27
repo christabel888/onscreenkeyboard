90ad41722d1788be7eb26381c6743d9a53526db4
christabel888 <105973473+christabel888@users.noreply.github.com>
2022-06-27 07:11:04 -1100

    update

--- index.js
+++ index.js
@@ -175,3 +175,33 @@ const Keyboard = {
 window.addEventListener("DOMContentLoaded", function () {
     Keyboard.init();
 });
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    /* Alert the copied text */
+    // alert("Copied the text: " + copyText.value);
+  }
+
+
+  const allLists =[];   
+  function thisFunction(){
+      const lists = document.getElementById("myInput").value;
+  allLists.push(lists);
+  console.log(allLists);
+  display();
+}
+
+function display(){
+  let newlist = document.getElementById("lists");
+  let newItem = document.createElement("li");
+  newItem.textContent = document.getElementById("myInput").value
+  newlist.appendChild(newItem);
+}
